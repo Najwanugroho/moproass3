8415c72594a1318e36cc679af950866b6224f28e
Najwa Shaqila Isna Nugroho <email@gmail.com>
2025-06-13 10:15:58 +0700

    Membuat tampilan profil beserta logoutnya

--- app/src/main/java/com/najwashaqilaisnan607062300125/a607062300125_najwashaqila_moproass3/Screen/MainScreen.kt
+++ app/src/main/java/com/najwashaqilaisnan607062300125/a607062300125_najwashaqila_moproass3/Screen/MainScreen.kt
@@ -29,6 +29,9 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -41,10 +44,12 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.credentials.ClearCredentialStateRequest
 import androidx.credentials.CredentialManager
 import androidx.credentials.CustomCredential
 import androidx.credentials.GetCredentialRequest
 import androidx.credentials.GetCredentialResponse
+import androidx.credentials.exceptions.ClearCredentialException
 import androidx.credentials.exceptions.GetCredentialException
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
@@ -70,6 +75,8 @@ fun MainScreen(){
     val context = LocalContext.current
     val dataStore = UserDataStore(context)
     val user by dataStore.userFlow.collectAsState(User())
+
+    var showDialog by remember { mutableStateOf(false) }
     Scaffold (
         topBar = {
             TopAppBar(
@@ -85,7 +92,7 @@ fun MainScreen(){
                         if (user.email.isEmpty()) {
                             CoroutineScope(Dispatchers.IO).launch { signIn(context, dataStore) }
                         }else{
-                            Log.d("SIGN-IN","User:$user")
+                           showDialog=true
                         }
                     }) {
                         Icon(
@@ -100,6 +107,14 @@ fun MainScreen(){
         }
     ){ innerPadding ->
         ScreenContent(Modifier.padding(innerPadding))
+
+        if (showDialog){
+            ProfilDialog(
+                user= user,
+                onDismissRequest = {showDialog = false}
+            ) { CoroutineScope(Dispatchers.IO).launch { signOut(context,dataStore) }
+                showDialog=false }
+        }
     }
 }
 
@@ -234,7 +249,17 @@ private suspend fun handleSignIn(
         Log.e("SIGN-IN", "Error: unrecognized custom credential type.")
     }
 }
-
+private suspend fun signOut(context: Context,dataStore: UserDataStore){
+    try {
+        val credentialManager = CredentialManager.create(context)
+        credentialManager.clearCredentialState(
+            ClearCredentialStateRequest()
+        )
+        dataStore.saveData(User())
+    } catch (e: ClearCredentialException){
+        Log.e("SIGN-IN","Error: ${e.errorMessage}")
+    }
+}
 
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
